9fbd1484c2598c4539a3af13a5d21b32efd9c008
maksymrozvodovskyi <152100542+maksymrozvodovskyi@users.noreply.github.com>
2024-02-27 17:22:55 +0300

    Update newfile.cpp

--- newfile.cpp
+++ newfile.cpp
@@ -3,39 +3,26 @@ using namespace std;
 
 int main()
 {
+	cout << "enter number: ";
 
-int N;
-cin >> N;
+	int N = 0;
+	cin >> N;
 
-for(int i = 0; i < N; i++)
-{
-
-for ( int j = 10; j > i; j--)
-{
-    cout << " ";
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = N; j >= i; j--)
+		{
+			cout << " ";
+		}
+		for (int j = 0; j <= i; j++)
+		{
+			cout << i;
+		}
+		for (int j = 0; j < i; j++)
+		{
+			cout << j;
+		}
+		cout << endl;
+	}
 }
 
-for ( int j = 0; j <= i; j++)
-{
-    cout << i;
-}
-
-for (int j = 0; j < i; j++)
-{
-    cout << j;
-}
-cout << endl;
-
-}
-
-
-
-
-
-
-
-
-
-
-
-}
