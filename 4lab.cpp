14e58607c7b4c5196332d31bddf560ef113c0c60
maksymrozvodovskyi <152100542+maksymrozvodovskyi@users.noreply.github.com>
2024-04-17 13:50:53 +0300

    Update 4lab.cpp

--- 4lab.cpp
+++ 4lab.cpp
@@ -1,82 +1,128 @@
-#include ,iostream.
+#include <iostream>
 using namespace std;
 
 class Transport
 {
-
 private:
-bool p;
-float speed;
+    bool p;
+    float speed;
 
 public:
-void SetSpeed(newspeed){speed = newspeed;}
-float GetSpeed(){return speed;}
+    void SetSpeed(float newspeed)
+    {
+        speed = newspeed;
+    }
 
-void Move(float speed)
-{   if ( speed > 0)
+    float GetSpeed()
     {
-    p = true;
-   if (p)
-   {
-    cout << "Car is moving";
-   }
+        return speed;
     }
-}
 
-void Brake(float speed)
-{
-  if ( speed <= 0)
+    void Move(float speed)
     {
-    p = false;
-   if (!p)
+        if (speed > 0)
+        {
+            p = true;
+            if (p)
+            {
+                cout << "Wroten speed is: " << speed << " - Car is moving" << endl << endl;
+            }
+        }
+    }
+
+    void Brake(float speed)
     {
-    cout << "The car is brake";
+        if (speed <= 0)
+        {
+            p = false;
+            if (!p)
+            {
+                cout << "Oooops, The car is brake!! Check it please" << endl << endl;
+            }
+        }
     }
-    }
-}
-}
+};
 
 class Car : public Transport
 {
 
 private:
-string make;
-string model;
-int year;
+    string make;
+    string model;
+    int year;
+    string color; 
 
 public:
-void SetMake(newmake){make = newmake;}
-string GetMake(){return make;}
+    void SetMake(string newmake)
+    {
+        make = newmake;
+    }
 
-void SetModel(newmodel){model = newmodel;}
-string GetModel(){return model;}
+    string GetMake()
+    {
+        return make;
+    }
 
-void SetYear(newyear){year = newyear;}
-string GetYear(){return year;}
+    void SetModel(string newmodel)
+    {
+        model = newmodel;
+    }
 
-void Info()
-{
-    cout << "The car has make: " << make << endl;
-    cout << "The car has model: " << model << endl;
-    cout << "The car has year: " << year << endl;
-}
-}
+    string GetModel()
+    {
+        return model;
+    }
 
+    void SetYear(int newyear)
+    {
+        year = newyear;
+    }
 
+    int GetYear()
+    {
+        return year;
+    }
+
+    void SetColor(string newcolor)
+    {
+        color = newcolor;
+    }
+
+    string GetColor()
+    {
+        return color;
+    }
+
+    void Info()
+    {
+        cout << "The car has make: " << make << endl;
+        cout << "The car has model: " << model << endl;
+        cout << "The car has year: " << year << endl;
+        cout << "The car has color: " << color << endl << endl;
+    }
+
+    void Honk()
+    {
+        cout << "Beep! Beep! " << endl << endl;
+    }
+};
 
 int main()
 {
+    Car myCar;
 
+    myCar.SetMake("Honda");
+    myCar.SetModel("Accord");
+    myCar.SetYear(2023);
+    myCar.SetColor("Blue");
 
+    myCar.Info();
 
+    myCar.Move(80.0);
+    myCar.Brake(0.0);
 
+    myCar.Honk();
 
-
-
-
-
-
-
-
-
+    return 0;
 }
+
